c66c9396baf7d7258a99aaf3c15bf95142dd8e56
Emmanuel Opare <emmanuelopare763@gmail.com>
2025-02-07 11:11:47 +0000

    initial commit

--- alx-movie-app/interfaces/index.ts
+++ alx-movie-app/interfaces/index.ts
@@ -1,36 +1,36 @@
 import { ReactNode } from "react";
 
 export interface ComponentProps {
-    children: ReactNode;
+  children: ReactNode
 }
 
 export interface ButtonProps {
-    title: string;
-    action?: () => void;
+  title: string
+  action?: () => void
 }
 
 export interface MovieProps {
-    id?: string;
-    posterImage: string;
-    releaseYear: string;
-    title: string;
+  id?: string
+  posterImage: string
+  releaseYear: string
+  title: string
 }
 
-interface PosterPath {
-    url: string;
+interface PrimaryImage {
+  url: string
 }
 
-interface Title {
-    text: string;
+interface TitleText {
+  text: string
 }
 
-interface ReleaseDate {
-    year: string;
+interface ReleaseYear {
+  year: string
 }
 
 export interface MoviesProps {
-    id: string;
-    poster_path: string;
-    title: string;
-    release_date: string;
+  id: string
+  primaryImage: PrimaryImage
+  titleText: TitleText
+  releaseYear: ReleaseYear
 }
--- alx-movie-app/pages/api/fetch-movies.ts
+++ alx-movie-app/pages/api/fetch-movies.ts
@@ -1,36 +1,31 @@
 import { MoviesProps } from "@/interfaces";
 import { NextApiRequest, NextApiResponse } from "next";
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
 
-export default async function handler(
-    request: NextApiRequest,
-    response: NextApiResponse
-) {
     if (request.method === "POST") {
         const { year, page, genre } = request.body;
         const date = new Date();
-        
-        try {
-            const apiKey = process.env.MOVIE_API_KEY; 
-            const resp = await fetch(
-              `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&page=${page}&year=${
-        year || date.getFullYear() }&sort=year.decr&limit=12&page=${page}&${genre && `genre=${genre}`}`
-            );
-
-            if (!resp.ok) {
-                throw new Error(`Failed to fetch movies. Status: ${resp.status}`);
+        const resp = await fetch(
+            `https://moviesdatabase.p.rapidapi.com/titles?year=${year || date.getFullYear()
+            }&sort=year.decr&limit=12&page=${page}&${genre && `genre=${genre}`}`,
+            {
+                headers: {
+                    "x-rapidapi-host": "moviesdatabase.p.rapidapi.com",
+                    "x-rapidapi-key": `${process.env.MOVIE_API_KEY}`,
+                },
             }
+        );
 
-            const moviesResponse = await resp.json();
-            const movies: MoviesProps[] = moviesResponse.results;
+        if (!resp.ok) throw new Error("Failed to fetch movies");
 
-            return response.status(200).json({
-                movies,
-            });
-        } catch (error) {
-            return response.status(500).json({ error: error.message });
-        }
+        const moviesResponse = await resp.json();
+        const movies: MoviesProps[] = moviesResponse.results;
+
+        return response.status(200).json({
+            movies,
+        });
     } else {
-        response.setHeader("Allow", ["POST"]);
-        response.status(405).end(`Method ${request.method} Not Allowed`);
+        response.setHeader('Allow', ['POST']);
+        response.status(405).end(`Method ${request.method} Not Allowed in here`);
     }
-}
+};
